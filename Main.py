2569219fc4da756252fc34a1dc69d27445091282
Akshat <33050725+aaaakshat@users.noreply.github.com>
2018-12-25 10:32:47 +0800

    Added intro banner

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
-
+# Intro banner - looks so cool
+from pyfiglet import Figlet
+textToPrint = Figlet(font="banner3")
+print(textToPrint.renderText("Tachyon"))
