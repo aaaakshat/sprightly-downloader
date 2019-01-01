9b7e4da616b0342973f704c269af257738b59093
Akshat <33050725+aaaakshat@users.noreply.github.com>
2019-01-01 15:42:11 +0800

    Updates

--- test.py
+++ test.py
@@ -1,21 +1,29 @@
-from threading import Thread
+import os
+import threading
 import time
 from tqdm import tqdm
 
+'''
+def threaded_function(chunk_size):
+    desktopPath = os.path.join(os.path.expanduser("~"), "Desktop")
+    filePath = os.path.join(desktopPath, "touch.txt")
+    outputFile = open(filePath, mode='a')
 
-global storage
-store = {}
+    for i in range(chunk_size):
+        outputFile.write("1")
 
-def threaded_function(arg):
-
+    outputFile.close()
 
 if __name__ == "__main__":
     global thread
     old = time.time()
 
     t = []
-    for i in range(10):
-        thread = Thread(target = threaded_function, args = (100000,), i*chunk_size)
+    threadnum = 20
+    finalsize = 81500000
+    chunksize = finalsize/threadnum
+    for i in range(threadnum):
+        thread =  threading.Thread(target=threaded_function, args=(int(chunksize),))
         t.append(thread)
         thread.start()
 
@@ -23,4 +31,29 @@ if __name__ == "__main__":
         thread.join()
 
     now = time.time()
-    print(now-old)
+    print("bytes written: %s threads: %s time taken: %s" % (finalsize, threadnum, now-old))
+'''
+
+def run():
+    '''
+    oldTime = time.time()
+
+    downloadPath = os.path.join(os.path.join(os.path.expanduser("~"), "Desktop"), "tachyon")
+    pathToWriteTo = os.path.join(os.path.join(os.path.expanduser("~"), "Desktop"), "tothisfolder")
+
+    for i in range(0,20):
+        os.rename(os.path.join(downloadPath, "text" + str(i) + ".txt"), os.path.join(pathToWriteTo, "text" + str(i) + ".txt"))
+
+    print(time.time() - oldTime)
+    '''
+    old = time.time()
+    downloadPath = os.path.join(os.path.join(os.path.expanduser("~"), "Desktop"), "tachyon")
+    myFile = os.path.join(downloadPath, "text0.txt")
+    outputFile = os.path.join(downloadPath, "text1.txt")
+
+    with open(myFile, "a") as file:
+        for i in range(0, 20):
+            file.write(open(os.path.join(downloadPath, "text" + str(i) + ".txt"), "r").read())
+    print(time.time() - old)
+
+run()
